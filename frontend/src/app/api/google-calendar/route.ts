a13a6c9a41b75b86fd5c5be6a7b269727f6b7e71
dmo7567 <daniel.morales@yale.edu>
2024-10-12 15:59:49 -0400

    Fixed GCal issues stemming from invalid string to date conversions.

--- frontend/src/app/api/google-calendar/route.ts
+++ frontend/src/app/api/google-calendar/route.ts
@@ -4,9 +4,6 @@ import { google } from 'googleapis';
 export async function POST(req: Request) {
   const { accessToken, match } = await req.json();
 
-  // Log the incoming request body to check its structure
-  console.log('Received request body:', { accessToken, match });
-
   try {
     // Initialize OAuth2 client and set credentials
     const oauth2Client = new google.auth.OAuth2();
