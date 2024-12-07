62a5b9327c914053064a17703c858d574c899f8a
kjoikle <60455201+kjoikle@users.noreply.github.com>
2024-12-07 13:45:35 -0500

    mobile footer

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -17,24 +17,25 @@ function Footer() {
     <div className="p-5 bg-blue-500 text-white">
       <div className="flex pb-5 justify-between">
         <p>Yale IMs &copy; 2024</p>
-        <div className="flex flex-row space-x-10">
-          {links.map((link, index) => (
-            <Link
-              key={link.href}
-              href={link.href}
-              className="hover:text-slate-300 flex justify-between items-center pl-4 pr-4"
-              onClick={resetFilters}
-            >
-              <div className=" ">{link.text}</div>
-            </Link>
-          ))}
+        <div className="flex flex-row space-x-2">
+          <a href="https://yalecomputersociety.org">YCS </a>
+          <p> x </p>
+          <a href="https://designatyale.com"> day</a>
         </div>
       </div>
-      <div className="flex flex-row space-x-2 pb-5">
-        <a href="https://yalecomputersociety.org">YCS </a>
-        <p> x </p>
-        <a href="https://designatyale.com"> day</a>
+      <div className="flex flex-row xs:space-x-10 pb-5 justify-center">
+        {links.map((link, index) => (
+          <Link
+            key={link.href}
+            href={link.href}
+            className="hover:text-slate-300 flex justify-between items-center pl-4 pr-4"
+            onClick={resetFilters}
+          >
+            <div className=" ">{link.text}</div>
+          </Link>
+        ))}
       </div>
+
       <div className="flex justify-center">
         <img src="/LOGO.png" />
       </div>
