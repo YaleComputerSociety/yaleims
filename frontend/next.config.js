6d35803d2a51c3eae382f33e765e296f1af50dc4
dennisephraim <akainetteyephraim@gmail.com>
2025-03-28 19:55:13 -0400

    init apphosting

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,11 +1,5 @@
 // next.config.js
 module.exports = {
-  typescript: {
-    ignoreBuildErrors: true,
-  },
-  eslint: {
-    ignoreBuildErrors: true,
-  },
   images: {
     domains: ["yims-125a2.web.app"],
     unoptimized: true, // Disable image optimization
