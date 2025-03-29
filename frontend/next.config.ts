43758b4e48d0ebc846ac26b27972578eb0c55044
dennisephraim <akainetteyephraim@gmail.com>
2025-03-29 13:29:49 -0400

    testing new frontend deployment

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,10 +1,8 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  images: {
-    domains: ["yims-125a2.web.app"],
-    unoptimized: true, 
-  },
+  
+  
 };
 
 export default nextConfig;
