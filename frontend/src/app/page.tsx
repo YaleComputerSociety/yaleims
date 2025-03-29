2e9cbe60f387e39d459f2c790220f88c947b57fe
dennisephraim <akainetteyephraim@gmail.com>
2025-03-28 20:04:23 -0400

    fixed folders

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,7 +1,7 @@
 "use client";
 
 import { useEffect, useState } from "react";
-import AAHomeComponent from "../components/home/AAHomeComponent";
+import AAHomeComponent from "@src/components/Home/AAHomeComponent"
 import Link from "next/link";
 
 const HomePage: React.FC = () => {
