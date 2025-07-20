83ccf41e2d4bf66a2d2d5739a688c3b3bd4ac525
Kaitlyn <kjoikle@gmail.com>
2025-07-20 13:32:48 -0400

    small fix

--- frontend/src/app/dashboard/page.tsx
+++ frontend/src/app/dashboard/page.tsx
@@ -17,7 +17,6 @@ const Dashboard: React.FC = () => {
       <PageHeading heading="Dashboard" />
       <div>
         {user?.role === "admin" && <AdminDashboard />}
-        <AdminDashboard />
         {user?.role === "user" && <UserDashboard />}
         {user?.role === "captain" && <CaptainDashboard />}
         {user?.role === "college_rep" && <CollegeRepDashboard />}
