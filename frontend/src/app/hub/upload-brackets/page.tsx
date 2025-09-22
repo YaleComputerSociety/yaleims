0d1e83131aa2265fc86c974eb83bab23193c6556
Kaitlyn <kjoikle@gmail.com>
2025-09-22 15:32:53 -0400

    add page heading

--- frontend/src/app/hub/upload-brackets/page.tsx
+++ frontend/src/app/hub/upload-brackets/page.tsx
@@ -1,12 +1,14 @@
 "use client";
 
 import BracketAdminPanel from "@src/components/Dashboard/Admin/BracketAdminPanel";
+import PageHeading from "@src/components/PageHeading";
 import withRoleProtectedRoute from "@src/components/withRoleProtectedRoute";
 import React from "react";
 
 const UploadBracketsPage = () => {
   return (
     <div className="min-h-screen pt-20">
+      <PageHeading heading="Upload/Delete Brackets" />
       <BracketAdminPanel />
     </div>
   );
