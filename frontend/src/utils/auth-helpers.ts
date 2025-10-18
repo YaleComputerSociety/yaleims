8aa307e3a571e95fba3395acd7fff00d1e190144
Kaitlyn <kjoikle@gmail.com>
2025-10-18 09:56:18 -0400

    added auth helper

--- frontend/src/utils/auth-helpers.ts
+++ frontend/src/utils/auth-helpers.ts
@@ -1,6 +1,30 @@
 import { cookies } from "next/headers";
 import jwt from "jsonwebtoken";
 
+interface DecodedToken {
+  name: string;
+  netid: string;
+  email: string;
+  role: string;
+  mRoles: string[];
+  username: string;
+  college: string;
+  points: number;
+  matches_played: number;
+}
+
+const decodedTokenShape: DecodedToken = {
+  name: "",
+  netid: "",
+  email: "",
+  role: "",
+  mRoles: [],
+  username: "",
+  college: "",
+  points: 0,
+  matches_played: 0,
+};
+
 /**
  * Helper function to check if the current user has one of the required roles.
  * @param requiredRoles
@@ -16,7 +40,21 @@ export const userTokenHasRoles = async (
   }
 
   const secret = process.env.JWT_SECRET as string;
-  const decoded = jwt.verify(token.value, secret) as { [key: string]: any };
+  const decoded = jwt.verify(token.value, secret) as DecodedToken;
+
+  if (!isValidDecodedToken(decoded)) {
+    return false;
+  }
 
   return requiredRoles.some((role) => decoded.mRoles.includes(role));
+};
+
+const isValidDecodedToken = (decoded: any): boolean => {
+  if (typeof decoded !== "object" || decoded === null) {
+    return false;
+  }
+
+  const requiredFields = Object.keys(decodedTokenShape);
+
+  return requiredFields.every((field) => field in decoded);
 };
